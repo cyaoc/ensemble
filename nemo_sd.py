a759a88714a9ffd84d63ced05e4b6bba10aa4b32
cyaoc <cyao@cybozu.cn>
2023-10-12 20:53:23 +0800

    add work home

--- nemo_sd.py
+++ nemo_sd.py
@@ -4,23 +4,17 @@ import wget
 from omegaconf import OmegaConf
 from nemo.collections.asr.models.msdd_models import NeuralDiarizer
 import torch
-
 import os
-
-def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
+import shutil
 
 def diarize(audio_file, config_type):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     file_dir = os.path.dirname(audio_file)
     file_name, _ = os.path.splitext(audio_file)
-    rttm = f'{file_name}.rttm'
+    work_home = os.path.join(file_dir, "nemo")
+    if not os.path.exists(work_home):
+        os.makedirs(work_home)
+
     meta = {
         'audio_filepath': f'{audio_file}',
         'offset': 0,
@@ -28,36 +22,28 @@ def diarize(audio_file, config_type):
         'label': "infer",
         'text': "-",
         'num_speakers': None,
-        'rttm_filepath': f'{rttm}',
+        'rttm_filepath': None,
         'uniq_id': ""
     }
-    print(audio_file)
-    print(rttm)
-    manifest = os.path.join(file_dir, 'manifest.json')
-    print(manifest)
+    manifest = os.path.join(work_home, 'manifest.json')
     if not os.path.exists(manifest):
         with open(manifest, 'w') as f:
             f.write(json.dumps(meta))   
-
-    mode_config = os.path.join(file_dir, f'diar_infer_{config_type}.yaml')
-    print(mode_config)
+    mode_config = os.path.join(work_home, f'diar_infer_{config_type}.yaml')
     if not os.path.exists(mode_config):
         config_url = f'https://raw.githubusercontent.com/NVIDIA/NeMo/main/examples/speaker_tasks/diarization/conf/inference/diar_infer_{config_type}.yaml'
-        mode_config = wget.download(config_url, file_dir)
-        print(mode_config)
+        mode_config = wget.download(config_url, work_home)
+
     config = OmegaConf.load(mode_config)
     config.num_workers = 1
-
     config.diarizer.manifest_filepath = manifest
-    config.diarizer.out_dir = os.path.join(file_dir, 'diarized')
-    print(config.diarizer.out_dir)
+    config.diarizer.out_dir = os.path.join(work_home, 'diarized')
     config.diarizer.vad.model_path = 'vad_multilingual_marblenet'
     config.diarizer.vad.parameters.onset = 0.8
     config.diarizer.vad.parameters.offset = 0.6
     config.diarizer.vad.parameters.pad_offset = -0.05
     config.diarizer.speaker_embeddings.model_path = 'titanet_large'
     config.diarizer.msdd_model.model_path = 'diar_msdd_telephonic'
-
     config.diarizer.oracle_vad = False
     config.diarizer.clustering.parameters.oracle_num_speakers=False
     model = NeuralDiarizer(cfg=config).to(device)
@@ -65,8 +51,12 @@ def diarize(audio_file, config_type):
     del model
     torch.cuda.empty_cache()
 
-    list_files(file_dir)
+    source_file = os.path.join(work_home, f"diarized/pred_rttms/{file_name}.rttm")
+    if os.path.exists(source_file):
+        shutil.move(source_file, file_dir) 
+        shutil.rmtree(work_home)
 
+    rttm = os.path.join(file_dir, f"{file_name}.rttm")
     with open(rttm, 'r') as file:
         text = file.read()
     return text, rttm
